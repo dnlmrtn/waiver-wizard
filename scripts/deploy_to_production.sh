9d0755d1d43fe91f4cacaa4af32a02577849ece5
dnlmrtn <danielmartin102001@gmail.com>
2024-12-20 17:12:23 -0500

    fix: changes to deployement script

--- scripts/deploy_to_production.sh
+++ scripts/deploy_to_production.sh
@@ -5,6 +5,6 @@ sudo docker compose -f docker-compose.prod.yaml build db redis celery celery-bea
 sudo docker compose -f docker-compose.prod.yaml restart db redis celery celery-beat app nginx
 sudo docker-compose -f $1 exec app python rcs/manage.py migrate_schemas
 
-docker system prune
+sudo docker system prune
 
 echo "Deployment Complete"
