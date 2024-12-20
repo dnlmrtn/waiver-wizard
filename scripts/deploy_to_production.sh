eb62e9fa5f27d1fb20a81e4d1b68ba2a0a2458a7
dnlmrtn <danielmartin102001@gmail.com>
2024-12-20 17:04:55 -0500

    Fix: Fixed incorrect branch name

--- scripts/deploy_to_production.sh
+++ scripts/deploy_to_production.sh
@@ -1,9 +1,9 @@
-git checkout master
-git pull origin master
+git checkout main
+git pull origin main
 
-docker compose -f docker-compose.prod.yaml build db redis celery celery-beat app nginx
-docker compose -f docker-compose.prod.yaml restart db redis celery celery-beat app nginx
-docker-compose -f $1 exec app python rcs/manage.py migrate_schemas
+sudo docker compose -f docker-compose.prod.yaml build db redis celery celery-beat app nginx
+sudo docker compose -f docker-compose.prod.yaml restart db redis celery celery-beat app nginx
+sudo docker-compose -f $1 exec app python rcs/manage.py migrate_schemas
 
 docker system prune
 
