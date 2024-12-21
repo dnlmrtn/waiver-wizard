3354681bdd65fcbd7979010eca761c51cce54758
dnlmrtn <danielmartin102001@gmail.com>
2024-12-21 14:33:00 -0500

    fix: removed old docker-compose ref in script

--- scripts/deploy_to_production.sh
+++ scripts/deploy_to_production.sh
@@ -3,7 +3,7 @@ git pull origin main
 
 sudo docker compose -f docker-compose.prod.yaml build db redis celery celery-beat app nginx
 sudo docker compose -f docker-compose.prod.yaml restart db redis celery celery-beat app nginx
-sudo docker-compose -f $1 exec app python rcs/manage.py migrate_schemas
+sudo docker compose -f docker-compose.prod.yaml exec app python rcs/manage.py migrate_schemas
 
 sudo docker system prune
 
